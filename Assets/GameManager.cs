479bad0348a281111d467322f0bb7c652f25104c
Liang Yu Lin <tanyalin2001@gmail.com>
2023-09-18 00:38:50 -0500

    Create different enemies and detect which to spawn when collide

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,4 +22,31 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance; // Singleton instance
+
+	public GameObject enemyToSpawn; // Store the collided enemy to spawn in the combat scene
+	public int playerHealth;
+
+	private void Awake()
+	{
+		Debug.Log("GameManager Awake called");
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
 }
