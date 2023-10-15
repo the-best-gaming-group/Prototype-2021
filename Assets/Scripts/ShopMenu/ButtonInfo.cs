c2a3a179d9d35b72940f712d002b559e625af8e3
Liang Yu Lin <tanyalin2001@gmail.com>
2023-10-14 21:14:24 -0500

    Shop Menu Setup

--- Assets/Scripts/ShopMenu/ButtonInfo.cs
+++ Assets/Scripts/ShopMenu/ButtonInfo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealthBar : MonoBehaviour
+public class ButtonInfo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
