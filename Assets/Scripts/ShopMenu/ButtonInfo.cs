7125ff7e09d9a5a8d7d5820375e55750c11b4fe8
Liang Yu Lin <tanyalin2001@gmail.com>
2023-10-15 02:00:25 -0500

    Basic shop system with spells and prices on

--- Assets/Scripts/ShopMenu/ButtonInfo.cs
+++ Assets/Scripts/ShopMenu/ButtonInfo.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int ItemID;
+	public TextMeshProUGUI PriceTxt;
+	public GameObject ShopManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+		PriceTxt.text = "Price: $" + ShopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
+	}
 }
