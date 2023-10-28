8a8a117cdf6a1276ab1c177f0b60c4dc1366da26
Liang Yu Lin <tanyalin2001@gmail.com>
2023-10-28 04:37:42 -0500

    Selection Menu done

--- Assets/SelectionManager.cs
+++ Assets/SelectionManager.cs
@@ -1,45 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using TMPro;
-
-//public class SelectionManager : MonoBehaviour
-//{
-//    public GameObject selectionPanel;
-//    public GameObject selectedPanel;
-//    public Button selectButton;
-//    public Button doneButton;
-//    public Button spellButtonPrefab;
-
-//    void Start()
-//    {
-//        PopulateSelectionPanel();
-//    }
-
-//    void PopulateSelectionPanel()
-//    {
-//        // Access GameManager.Instance to get the list of spells
-//        GameManager gameManager = GameManager.Instance;
-
-//        // Loop through the spells and create UI elements for each spell in the selection panel
-//        foreach (GameManager.Spell spell in gameManager.spells)
-//        {
-//            Debug.Log(spell.name);
-//            // Create a UI button for the spell
-//            Button spellButton = Instantiate(spellButtonPrefab, selectionPanel.transform);
-//            spellButton.GetComponentInChildren<TextMeshProUGUI>().text = spell.name;
-//            Debug.Log(spellButton.GetComponentInChildren<TextMeshProUGUI>().text);
-
-//            // Add an onClick listener to select the spell when the button is clicked
-//            //spellButton.onClick.AddListener(() => SelectSpell(spell));
-//        }
-//    }
-
-
-
-//}
-
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -49,8 +7,6 @@ public class SelectionManager : MonoBehaviour
 {
 	public GameObject selectionPanel;
 	public GameObject selectedPanel;
-	public Button selectButton;
-	public Button doneButton;
 	public Button spellButtonPrefab;
 
 	private List<Button> spellButtons = new List<Button>();
@@ -59,8 +15,6 @@ public class SelectionManager : MonoBehaviour
 	private void Start()
 	{
 		PopulateSelectionPanel();
-		selectButton.interactable = false; // Initially, the select button is disabled.
-		selectButton.onClick.AddListener(SelectDone);
 	}
 
 	private void PopulateSelectionPanel()
@@ -73,64 +27,44 @@ public class SelectionManager : MonoBehaviour
 		{
 			Button spellButton = Instantiate(spellButtonPrefab, selectionPanel.transform);
 			spellButton.GetComponentInChildren<TextMeshProUGUI>().text = spell.name;
-			spellButton.onClick.AddListener(() => SelectSpell(spellButton));
+			spellButton.onClick.AddListener(() => MoveToSelectedPanel(spellButton));
 			spellButtons.Add(spellButton);
 		}
 	}
 
-	public void SelectSpell(Button spellButton)
+	public void MoveToSelectedPanel(Button spellButton)
 	{
-		if (selectedSpells.Contains(spellButton))
+		if (selectedSpells.Count < 4)
 		{
-			// Deselect the spell if it's already selected
-			Debug.Log("Contain spell so remove in the list " + spellButton.GetComponentInChildren<TextMeshProUGUI>().text);
-			selectedSpells.Remove(spellButton);
-			// You can customize the appearance of deselected buttons here.
-		}
-		else if (selectedSpells.Count < 4)
-		{
-			// Select the spell if it's not already selected and there's room for more selections
-			Debug.Log("Add spell in the list " + spellButton.GetComponentInChildren<TextMeshProUGUI>().text);
-			selectedSpells.Add(spellButton);
-			// You can customize the appearance of selected buttons here.
-		}
-
-		// Enable the select button if there are selected spells, otherwise, keep it disabled
-		selectButton.interactable = selectedSpells.Count > 0;
-	}
-
-	public void SelectDone()
-	{
-		// Move selected spells from the selectionPanel to the selectedPanel
-		foreach (Button selectedSpell in selectedSpells)
-		{
-			// Remove the selected spell from the selectionPanel
-			spellButtons.Remove(selectedSpell);
-			Destroy(selectedSpell.gameObject);
+			spellButton.gameObject.SetActive(false);
 
 			// Add the selected spell to the selectedPanel
-			Button selectedSpellClone = Instantiate(selectedSpell, selectedPanel.transform);
-			selectedSpellClone.onClick.RemoveAllListeners(); // Remove the old click listeners.
-			selectedSpellClone.onClick.AddListener(() => DeselectSpell(selectedSpellClone));
+			Button selectedSpellClone = Instantiate(spellButtonPrefab, selectedPanel.transform);
+			TextMeshProUGUI cloneText = selectedSpellClone.GetComponentInChildren<TextMeshProUGUI>();
+			cloneText.text = spellButton.GetComponentInChildren<TextMeshProUGUI>().text;
+			selectedSpells.Add(selectedSpellClone);
+			selectedSpellClone.onClick.AddListener(() => MoveToSelectionPanel(selectedSpellClone));
 		}
-
-		// Clear the selected spells list
-		selectedSpells.Clear();
-
-		// Disable the select button since no spells are selected
-		selectButton.interactable = false;
 	}
 
-	public void DeselectSpell(Button selectedSpell)
+	public void MoveToSelectionPanel(Button selectedSpell)
 	{
-		// Move the deselected spell back to the selectionPanel
-		spellButtons.Add(selectedSpell);
+		// Remove the selected spell from the selectedPanel
+		selectedSpells.Remove(selectedSpell);
 		Destroy(selectedSpell.gameObject);
 
-		// Remove the deselected spell from the selectedPanel
-		selectedSpells.Remove(selectedSpell);
-
-		// Enable the select button if there is room for more selections
-		selectButton.interactable = selectedSpells.Count < 4;
+		// Find the corresponding spell in the selectionPanel and make it visible
+		foreach (Button spellButton in spellButtons)
+		{
+			Debug.Log("spellButton.GetComponentInChildren<TextMeshProUGUI>().text" + spellButton.GetComponentInChildren<TextMeshProUGUI>().text);
+			Debug.Log("selectedSpell.GetComponentInChildren<TextMeshProUGUI>().text" + selectedSpell.GetComponentInChildren<TextMeshProUGUI>().text);
+			if (spellButton.GetComponentInChildren<TextMeshProUGUI>().text == selectedSpell.GetComponentInChildren<TextMeshProUGUI>().text)
+			{
+				Debug.Log(spellButton.GetComponentInChildren<TextMeshProUGUI>().text);
+				spellButton.gameObject.SetActive(true);
+				break;
+			}
+		}
 	}
 }
+
