675b1442793ce894ce11d207d81e330a2716b287
BenP <ben.price2013@gmail.com>
2023-11-30 00:57:22 -0600

    Try to improve reroll button

--- Assets/Scripts/Mechanics/TurnBasedCombatUI/RerollController.cs
+++ Assets/Scripts/Mechanics/TurnBasedCombatUI/RerollController.cs
@@ -3,11 +3,18 @@ using UnityEngine.UI;
 
 public class RerollController : MonoBehaviour
 {
+    Button button;
+    public void Start()
+    {
+        button = transform.Find("Button").GetComponent<Button>();
+    }
     public void Disable()
     {
+        button.interactable = false;
     }
     
     public void Enable()
     {
+        button.interactable = true;
     }
 }
